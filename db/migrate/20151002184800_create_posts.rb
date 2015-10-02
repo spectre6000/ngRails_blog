26d15061307667124d1f3d067295ed85ca69000f
spectre6000 <spectre6000@gmail.com>
2015-10-02 12:51:54 -0600

    post migration

--- db/migrate/20151002184800_create_posts.rb
+++ db/migrate/20151002184800_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.date :release_date
 
       t.timestamps null: false
     end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "should not save post entry without any of title, body, or release date" do
+    post = Post.new(body: 'Test body', release_date: Time.now)
+    assert_not post.save
+    post = Post.new(title: 'Test', release_date: Time.now)
+    assert_not post.save
+    post = Post.new(title: 'Test', body: 'Test body')
+    assert_not post.save
+    post = Post.new(title: 'Test', body: 'Test body', release_date: Time.now)
+    assert post.save
+  end
 end
