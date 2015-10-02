0cbf832643fd8fd00adb5fe214beb708052197bf
spectre6000 <spectre6000@gmail.com>
2015-10-02 13:49:28 -0600

    posts factory in angular controller

--- app/assets/javascripts/angular/controllers/posts_controllers.js
+++ app/assets/javascripts/angular/controllers/posts_controllers.js
@@ -1,1 +1,18 @@
-var myApp = angular.module('myapplication' ['ngRoute', 'ngResource']);
+//module declaration
+var myApp = angular.module( 'myapplication', [ 'ngRoute', 'ngResource' ]);
+
+//Factory
+myApp.factory( 'Posts', [ '$resource', function( $resource ) {
+  return $resource('/posts.json', {}, {
+    query: { method: 'GET', isArray: true },
+    create: { method: 'POST' }
+  });
+}]);
+
+myApp.factory( 'Post', [ '$resource', function( $resource ){
+  return $resource( '/posts/:id.json', {} {
+    show: { method: 'GET' },
+    update: { method: 'PUT', params: { id: '@id' } },
+    delete: { method: 'DELETE', params: { id: '@id' } }
+  });
+}]);
